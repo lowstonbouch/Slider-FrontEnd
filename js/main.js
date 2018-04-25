5028329a95250f633d827a4600bca11043d9d263
lowstonbouch <lowstonbouch@gmail.com>
2018-04-25 13:57:03 +0300

    edit code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-let lis = document.getElementsByTagName('li');
 const carousel = document.getElementById('carousel');
 const photoWrapper = document.getElementById('photo-wrapper');
 const infoWrapper = document.getElementById('info-wrapper');
@@ -16,15 +15,15 @@ const countPhoto = document.getElementById('count-photo');
 const whatchMap = document.getElementById('whatch-map');
 
 carousel.addEventListener('click', carouselIvent);
-likeWrapper.addEventListener('click',handleLike);
-dislikeWrapper.addEventListener('click',handleDislike);
-superLikeWrapper.addEventListener('click',handleSuperLike);
-profileWrapper.addEventListener('click',handleProfile);
-prevButton.addEventListener('click',handlePrevButton);
-nextButton.addEventListener('click',handleNextButton);
-prevBigButton.addEventListener('click',handlePrevBigButton);
-nextBigButton.addEventListener('click',handleNextBigButton);
-whatchMap.addEventListener('click',handleWhatchMap);
+likeWrapper.addEventListener('click', handleLike);
+dislikeWrapper.addEventListener('click', handleDislike);
+superLikeWrapper.addEventListener('click', handleSuperLike);
+profileWrapper.addEventListener('click', handleProfile);
+prevButton.addEventListener('click', handlePrevButton);
+nextButton.addEventListener('click', handleNextButton);
+prevBigButton.addEventListener('click', handlePrevBigButton);
+nextBigButton.addEventListener('click', handleNextBigButton);
+whatchMap.addEventListener('click', handleWhatchMap);
 
 
 let numberPhoto = 0;
@@ -33,78 +32,75 @@ let like = false;
 let dislike = false;
 let superLike = false;
 
-function handleWhatchMap(event){
+function handleWhatchMap(event) {
   const target = event.target;
-  if(target.id = "whatch-map-button"){
+  if (target.id = "whatch-map-button") {
     whatchMap.className = 'none';
   }
 }
 
-function handlePrevButton(){
+function handlePrevButton() {
   let listElems = carousel.querySelectorAll('li');
   let list = carousel.querySelector('ul');
-  let width = listElems.length* 134 - 804;
+  let width = listElems.length * 134 - 804;
 
-  if(position < 0){
+  if (position < 0) {
     position = position + 134;
     list.style.marginLeft = position + 'px';
   }
 }
 
-function handleNextBigButton(){
+function handleNextBigButton() {
   carouselBigIvent('next');
 }
 
-function handlePrevBigButton(){
+function handlePrevBigButton() {
   carouselBigIvent('prev');
 }
 
-function handleNextButton(){
+function handleNextButton() {
   let listElems = carousel.querySelectorAll('li');
   let list = carousel.querySelector('ul');
-  let width = listElems.length* 134 - 804;
+  let width = listElems.length * 134 - 804;
 
-  if(position > -width){
+  if (position > -width) {
     position = position - 134;
     list.style.marginLeft = position + 'px';
   }
 }
 
-function handleProfile(){
+function handleProfile() {
   renderContent();
 }
 
-function handleSuperLike(){
+function handleSuperLike() {
   superLike = !superLike;
-  if(superLike){
+  if (superLike) {
     superLikeWrapper.className = 'super-like super-like-true';
     superLikeWrapper.classList.add('click');
-  }
-  else{
+  } else {
     superLikeWrapper.classList.remove('click');
     superLikeWrapper.className = 'super-like';
   }
 }
 
-function handleDislike(){
+function handleDislike() {
   dislike = !dislike;
-  if(dislike){
+  if (dislike) {
     dislikeWrapper.className = 'dislike dislike-true';
     dislikeWrapper.classList.add('click');
-  }
-  else{
+  } else {
     dislikeWrapper.classList.remove('click');
     dislikeWrapper.className = 'dislike';
   }
 }
 
-function handleLike(){
+function handleLike() {
   like = !like;
-  if(like){
+  if (like) {
     likeWrapper.className = 'like heart-true';
     likeWrapper.classList.add('click');
-  }
-  else{
+  } else {
     likeWrapper.classList.remove('click');
     likeWrapper.className = 'like';
   }
@@ -113,40 +109,40 @@ function handleLike(){
 function carouselIvent(event) {
   const target = event.target;
 
-  if(target.id.slice(0,5) === "photo"){
-    numberPhoto = target.id.slice(6,7);
+  if (target.id.slice(0, 5) === "photo") {
+    numberPhoto = target.id.slice(6, 7);
     renderBigPhoto();
   }
 }
 
-function renderBigPhoto(){
+function renderBigPhoto() {
   photoWrapper.className = 'none';
   infoWrapper.className = 'none';
   bigPhotoWrapper.className = "big-photo";
   addBigPhoto();
 }
 
-function renderContent(){
+function renderContent() {
   photoWrapper.className = 'photo-wrapper';
   infoWrapper.className = 'info-wrapper';
   deleteBigPhoto();
   bigPhotoWrapper.className = "none";
 }
 
-function addBigPhoto(){
+function addBigPhoto() {
   const div = document.getElementById('gallery-big');
   let img = document.createElement('img');
   img.id = 'big-photo';
   img.src = `./img/s${numberPhoto}.jpg`
   img.className = "big-photo";
   div.appendChild(img);
-  countPhoto.innerHTML=`${numberPhoto}/7`;
+  countPhoto.innerHTML = `${numberPhoto}/7`;
 }
 
-function deleteBigPhoto(){
+function deleteBigPhoto() {
   const div = document.getElementById('gallery-big');
   const img = document.getElementById('big-photo');
-  if(img){
+  if (img) {
     div.removeChild(img);
     bigGifts.className = 'none';
   }
@@ -155,26 +151,26 @@ function deleteBigPhoto(){
 
 function carouselBigIvent(event) {
   const image = document.getElementById('big-photo');
-  console.log(numberPhoto);
-  console.log(event);
-  if(event === "prev" && numberPhoto > 1){
-    image.src = `./img/s${--numberPhoto}.jpg`;
-    countPhoto.innerHTML=`${numberPhoto}/7`;
-  }
-  if(event === "next" && +numberPhoto === 7){
-    console.log('her');
-    numberPhoto = +numberPhoto + 1;
-  }
-  if(event === "next" && numberPhoto < 7){
-    image.src = `./img/s${++numberPhoto}.jpg`;
-    countPhoto.innerHTML=`${numberPhoto}/7`;
+  if(image){
+    if (event === "prev" && numberPhoto > 1) {
+      image.src = `./img/s${--numberPhoto}.jpg`;
+      countPhoto.innerHTML = `${numberPhoto}/7`;
+    }
+    if (event === "next" && +numberPhoto === 7) {
+      numberPhoto = +numberPhoto + 1;
+    }
+    if (event === "next" && numberPhoto < 7) {
+      image.src = `./img/s${++numberPhoto}.jpg`;
+      countPhoto.innerHTML = `${numberPhoto}/7`;
+    }
+  
+    if (numberPhoto > 7) {
+      bigGifts.className = 'big-gifts';
+    } else {
+      bigGifts.className = 'none';
+    }
   }
 
-  if(numberPhoto > 7){
-    bigGifts.className = 'big-gifts';
-  }else{
-    bigGifts.className = 'none';
-  }
 }
 
 function initMap() {
@@ -187,20 +183,26 @@ function initMap() {
   });
 }
 
-function moveRect(e){
-   
-  switch(e.keyCode){
-
-      case 32: carouselBigIvent('next'); break;   
-      case 37:  // если нажата клавиша влево
-        carouselBigIvent('prev');
-          break;
-      case 39:   // если нажата клавиша вправо
-        carouselBigIvent('next');
-          break;
-      case 49: handleLike(); break;
-      case 50: handleDislike(); break;
-      case 51: handleSuperLike(); break;
+function moveRect(e) {
+  switch (e.keyCode) {
+    case 32:
+      carouselBigIvent('next');
+      break;
+    case 37:
+      carouselBigIvent('prev');
+      break;
+    case 39:
+      carouselBigIvent('next');
+      break;
+    case 49:
+      handleLike();
+      break;
+    case 50:
+      handleDislike();
+      break;
+    case 51:
+      handleSuperLike();
+      break;
   }
 }
 
